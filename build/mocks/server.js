25e0dbafc201f8eb32cd6e125304f933b0c6397b
Dave Boyle <spektraldevelopment@gmail.com>
2025-03-05 10:58:49 -0500

    [main] - Added build folder to gitignore.

--- build/mocks/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_1 = require("msw/node");
-const handlers_1 = require("./handlers");
-const server = (0, node_1.setupServer)(...handlers_1.handlers);
-exports.default = server;
