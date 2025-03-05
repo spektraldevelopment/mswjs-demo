360d9f39ecd73448741abd100b7a154b6135de91
Dave Boyle <spektraldevelopment@gmail.com>
2025-03-05 11:24:14 -0500

    [user-id] - Switched to using mock user json file for getting users.

--- src/mocks/handlers/user.ts
+++ src/mocks/handlers/user.ts
@@ -1,13 +1,27 @@
 import { http, HttpResponse, RequestHandler } from "msw";
+import data from "../data/users.json";
+
+interface User {
+  id: string;
+  firstName?: string;
+  lastName?: string;
+}
 
 export const userHandlers: RequestHandler[] = [
   // Intercept "GET https://example.com/user" requests...
   http.get("https://example.com/user", (): HttpResponse => {
+
+    // * Get first user in array of users...
+    const user: User = data.users[0];
     // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      firstName: "John",
-      lastName: "Maverick",
-    });
+    return HttpResponse.json(user);
   }),
+  http.get('https://example.com/user/:id', ({ request, params, cookies }): HttpResponse => {
+    const { id } = params;
+
+    // Find user by ID in data.users array...
+    const user: User = data.users.find((user) => user.id === id)!;
+
+    return HttpResponse.json(user);
+  })
 ];
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,6 +1,8 @@
 import { setupServer, SetupServerApi } from "msw/node";
 import { handlers } from "./handlers";
 
+// console.log("Handlers List: ", handlers);
+
 const server: SetupServerApi = setupServer(...handlers);
 
 export default server;
