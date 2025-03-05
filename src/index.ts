2dbb51a48a442972df645a479f2a3b9b5991d925
Dave Boyle <spektraldevelopment@gmail.com>
2025-03-05 12:00:41 -0500

    [post-example] - Added post example

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,51 @@ server.listen();
 
 // This is a simple Node.js application that
 // does a network request and prints the response.
-const app = () => {
+const app = async () => {
   fetchUser();
   fetchTestUser();
   fetchAllUsers();
+  await postNewUser();
 }
+
+const log = (message: string, obj: {} | []) => {
+  console.log("************************************");
+  console.log(`${message}`, obj);
+  console.log("************************************");
+};
 
 const fetchUser = async () => {
   const response = await fetch("https://example.com/user");
   const user = await response.json();
-  console.log("USER is: ", user);
+  log(`USER is: `, user);
 };
 
 const fetchTestUser = async () => {
   const response = await fetch("https://example.com/user/123");
   const user = await response.json();
-  console.log("TEST USER is: ", user);
+  log(`TEST USER is: `, user);
 };
 
 const fetchAllUsers = async () => {
   const response = await fetch("https://example.com/all-users");
   const { users } = await response.json();
-  console.log("USERS are: ", users);
+  log(`ALL USERS are: `, users);
+};
+
+const postNewUser = async () => {
+  const response = await fetch("https://example.com/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: "566778",
+      firstName: "John",
+      lastName: "Foo",
+    }),
+  });
+  const newUser = await response.json();
+  log(`NEW USER is: `, newUser);
 };
 
 app();
--- src/mocks/handlers/user.ts
+++ src/mocks/handlers/user.ts
@@ -3,25 +3,53 @@ import data from "../data/users.json";
 
 interface User {
   id: string;
-  firstName?: string;
-  lastName?: string;
+  firstName: string;
+  lastName: string;
 }
+
+// * Assign data to a new object in order to avoid mutation.
+const users = [...data.users];
 
 export const userHandlers: RequestHandler[] = [
   // Intercept "GET https://example.com/user" requests...
   http.get("https://example.com/user", (): HttpResponse => {
 
     // * Get first user in array of users...
-    const user: User = data.users[0];
+    const user: User = users[0];
     // ...and respond to them using this JSON response.
     return HttpResponse.json(user);
   }),
-  http.get('https://example.com/user/:id', ({ request, params, cookies }): HttpResponse => {
+  http.get('https://example.com/user/:id', ({ params }): HttpResponse => {
     const { id } = params;
 
-    // Find user by ID in data.users array...
-    const user: User = data.users.find((user) => user.id === id)!;
+    // Find user by ID in users array...
+    const user: User = users.find((user) => user.id === id)!;
 
     return HttpResponse.json(user);
-  })
+  }),
+  http.post('https://example.com/user', async ({ request }): Promise<HttpResponse> => {
+
+    const req = await request.json();
+
+    // * Extract new user from request body...
+    const newUser: User = req as User;
+
+    // * Add new user to users array...
+    users.push(newUser);
+
+    // * Respond with the new user.
+    return HttpResponse.json(newUser);
+  }),
+  // http.put('https://example.com/user/:id', async ({ request, params }): Promise<HttpResponse> => {
+  //   const { id } = params;
+  //   const req = await request.json();
+
+  //   // Find user by ID in users array...
+  //   const userIndex = users.findIndex((user) => user.id === id);
+
+  //   // Update user in the users array...
+  //   //users[userIndex] = updatedUser;
+
+  //   //return HttpResponse.json(updatedUser);
+  // }),
 ];
